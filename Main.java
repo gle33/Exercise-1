eab2494c88a6fdf8d5a567ff3dcc7a5f2e739a81
gle33 <gle3@ocdsb.ca>
2023-04-08 19:20:50 -0400

    Final touches

--- Main.java
+++ Main.java
@@ -6,32 +6,32 @@ import java.awt.event.*;  // actions for buttons
 import javax.swing.*; // import swing package
 public class Main{
     public static void main(String[] args){
-        JFrame main_frame = new JFrame("Exercise 1 - Is your number an integer?"); // make window
+        JFrame main_frame = new JFrame("Integer Verifier"); // make window
         main_frame.setSize(1000, 500); // set default size of window
 
         JLabel correct_input = new JLabel("It is an integer!");
-        correct_input.setBounds(10, 10, 300, 25);
-        correct_input.setBackground(Color.CYAN);
+        correct_input.setBounds(450, 250, 100, 25);
+        correct_input.setBackground(Color.GREEN);
         correct_input.setOpaque(true);
         main_frame.add(correct_input);
         correct_input.setVisible(false);
 
-        JLabel invalid_input = new JLabel("Not an integer - try again.");
-        invalid_input.setBounds(10, 10, 300, 25);
-        invalid_input.setBackground(Color.CYAN);
+        JLabel invalid_input = new JLabel("Not an integer.");
+        invalid_input.setBounds(450, 250, 100, 25);
+        invalid_input.setBackground(Color.RED);
         invalid_input.setOpaque(true);
         main_frame.add(invalid_input);
         invalid_input.setVisible(false);
 
         JLabel prompt = new JLabel("Enter an integer: ");
-        prompt.setBounds(500, 50, 200, 25);
+        prompt.setBounds(450, 50, 200, 25);
         main_frame.add(prompt);
 
         JTextField user_input = new JTextField();
-        user_input.setBounds(500, 100, 200, 25);
+        user_input.setBounds(400, 100, 200, 25);
 
         JButton verify_button = new JButton("Verify"); // make button
-        verify_button.setBounds(200, 200, 100, 60); // x-coord, y-coord, width, height
+        verify_button.setBounds(300, 150, 100, 60); // x-coord, y-coord, width, height
         verify_button.addActionListener(new ActionListener(){ // what occurs if a button is pressed/an action is performed
             public void actionPerformed(ActionEvent e){
                 String user_num_string = user_input.getText(); // whatever user typed in field becomes a string
@@ -51,7 +51,7 @@ public class Main{
             }
         });
         JButton exit_button = new JButton("Exit");
-        exit_button.setBounds(700, 200, 100, 60);
+        exit_button.setBounds(600, 150, 100, 60);
         exit_button.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 main_frame.dispose();
